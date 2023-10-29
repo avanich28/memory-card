9158452f668499d763f1fec7503a7d9be9ca1fa7
avanich28 <arpawan.v@gmail.com>
2023-10-29 17:41:45 +0700

    change game result images

--- src/components/GameResult.jsx
+++ src/components/GameResult.jsx
@@ -1,6 +1,6 @@
 import styles from "../styles/GameResult.module.css";
-import ditto from "../assets/ditto.png";
-import voltorb from "../assets/voltorb.png";
+import pikachuDance from "../assets/pikachu-dance.gif";
+import pikachuDizzy from "../assets/pikachu-dizzy.webp";
 import Button from "./Button";
 
 function GameResult({ result, score }) {
@@ -10,8 +10,20 @@ function GameResult({ result, score }) {
         {result === "win" && <h2>You Win!</h2>}
         {result === "lose" && <h2>Game Over!</h2>}
         <div>
-          {result === "win" && <img src={ditto} alt="Ditto" />}
-          {result === "lose" && <img src={voltorb} alt="Voltorb" />}
+          {result === "win" && (
+            <img
+              src={pikachuDance}
+              alt="Pikachu Dance"
+              className={styles.winImg}
+            />
+          )}
+          {result === "lose" && (
+            <img
+              src={pikachuDizzy}
+              alt="Pikachu dizzy eyes"
+              className={styles.loseImg}
+            />
+          )}
         </div>
         <p>Your final score is {score}.</p>
         <div>
